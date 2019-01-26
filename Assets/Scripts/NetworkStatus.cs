329e53434aec327df42814a7e05f6bdbb3443b94
Alex Carlson <alex@acwd.me>
2019-01-26 14:58:00 -0600

    player stuff with networking

---
+++ Assets/Scripts/NetworkStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class NetworkStatus : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Text>().text = "connected players: " + PhotonNetwork.CountOfPlayers.ToString();
+    }
+}
