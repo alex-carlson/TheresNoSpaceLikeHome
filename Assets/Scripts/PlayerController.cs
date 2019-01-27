00a5a7cf0828512af3a9d7169e700029e28645c4
Alex Carlson <alex@acwd.me>
2019-01-27 14:04:52 -0600

    player and ui updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviourPun
     public float maxGravDist = 4f;
     public float maxGravity = 35f;
     public float planetDiameter;
+    public float JumpAnimTimeOffset = 1;
     
     private Vector2 _direction;
     private GameObject[] planets;
@@ -22,6 +23,8 @@ public class PlayerController : MonoBehaviourPun
     private Camera _cam;
     private float currentUpwardForce = 0;
     private bool active = false;
+    private Animator anim;
+    private SpriteRenderer sprite;
 
     private void Start()
     {
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviourPun
 
         _cam = GameObject.FindObjectOfType<Camera>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
 
         SetSpawnPosition();
 
@@ -46,6 +51,7 @@ public class PlayerController : MonoBehaviourPun
 
     private void SetSpawnPosition(){
         LevelGenerator lg = FindObjectOfType<LevelGenerator>();
+        Debug.Log("Moving to : " + lg.NextPlayerSpawnPoint.position);
         transform.position = lg.NextPlayerSpawnPoint.position;
         lg.MoveSpawn();
     }
@@ -76,32 +82,43 @@ public class PlayerController : MonoBehaviourPun
 
         var dir = pullTarget.position - transform.position;
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Quaternion q = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), (Time.fixedDeltaTime * 2));
+        Quaternion q = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle + 90, Vector3.forward), (Time.fixedDeltaTime * 5f));
         transform.rotation = q;
     }
 
     public void MoveLeft(){
         CheckPlayer();
-        _direction = -Vector2.up;
+        _direction = -Vector2.right;
+        anim.SetFloat("moveSpeed", 1);
+        sprite.flipX = false;
     }
 
     public void MoveRight(){
         CheckPlayer();
-        _direction = Vector2.up;
+        _direction = Vector2.right;
+        anim.SetFloat("moveSpeed", 1);
+        sprite.flipX = true;
     }
 
     public void ClearMovement(){
         CheckPlayer();
         _direction = Vector2.zero;
         currentUpwardForce = 0;
+        anim.SetFloat("moveSpeed", 0);
+        anim.SetBool("jumping", false);
     }
 
     public void Jump(){
         CheckPlayer();
+        anim.SetBool("jumping", true);
+        Invoke("PushOff", JumpAnimTimeOffset);
+    }
+
+    private void PushOff(){
         float dist = Vector3.Distance(transform.position, pullTarget.position);
-        
-        if(dist < planetDiameter)
-            rb.AddForce((-transform.right * jumpForce), ForceMode2D.Impulse);
+
+        if (dist < planetDiameter)
+            rb.AddForce((transform.up * jumpForce), ForceMode2D.Impulse);
     }
 
     private void CheckPlayer(){
---
+++ Assets/Scripts/TouchControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch myTouch = Input.GetTouch(0);
+
+        Touch[] myTouches = Input.touches;
+
+        if(myTouches.Length <= 0) return;
+        
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            //Do something with the touches
+            Debug.Log(myTouches[i].position.x);
+        }
+    }
+}
