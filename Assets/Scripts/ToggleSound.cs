dff450b04c1cb2dc39b780be23dc0ea40dc735e3
Alex Carlson <alex@acwd.me>
2019-01-27 17:08:42 -0600

    added volume

---
+++ Assets/Scripts/ToggleSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSound : MonoBehaviour
+{
+
+    public bool soundIsOn = true;
+    public AudioSource music;
+
+    public void Toggle(){
+        soundIsOn = !soundIsOn;
+        if(soundIsOn){
+            music.volume = 1;
+        } else {
+            music.volume = 0;
+        }
+    }
+}
